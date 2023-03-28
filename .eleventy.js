cb0b99f7acd06724929cd6c1294c7cc9b3470884
Sam O’Neill <sjoneill4@gmail.com>
2023-03-28 15:23:44 +0100

    Add syntax highlighting module

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+  // Add plugins
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   return {
     dir: {
       input: "src",
